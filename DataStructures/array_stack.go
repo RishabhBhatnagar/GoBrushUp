b41d175590301622cf6a3a8819dce2b0bee8f61f
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-12-20 12:52:27 +0530

    Clean DataStructures/array_stack.go

--- DataStructures/array_stack.go
+++ DataStructures/array_stack.go
@@ -2,38 +2,33 @@
 
 package main
 
-
-type Stack struct{
-    top int
-    stack_size int
-    arr []int
+type stack struct {
+	top       int
+	stackSize int
+	arr       []int
 }
 
-
-func (st *Stack) pop() int{
-    if st.top < 1 {
-        panic("StackUnderflow error: Can't pop from an empty stack")
-    } else{
-        st_top_value := st.arr[st.top-1]   // since array index started from 0
-        st.top -= 1;
-        return st_top_value
-    }
-    return -1  // error condition.
+func (st *stack) pop() int {
+	if st.top < 1
+		panic("StackUnderflow error: Can't pop from an empty stack")
+	else {
+		stTopValue := st.arr[st.top-1] // since array index started from 0
+		st.top--
+		return stTopValue
+	}
 }
 
-
-func (st *Stack) push(element int){
-    if st.top >= st.stack_size {
-        panic("StackUnderFlow error: Cannot push on a full stack")
-    } else {
-        st.arr = append(st.arr, element)
-        st.top += 1
-    }
+func (st *stack) push(element int) {
+	if st.top >= st.stackSize {
+		panic("StackUnderFlow error: Cannot push on a full stack")
+	} else {
+		st.arr = append(st.arr, element)
+		st.top++
+	}
 }
 
-
-func main(){
-    st := Stack{stack_size: 10}
-    st.push(23)
-    st.pop()               // returns 23.
+func main() {
+	st := stack{stackSize: 10}
+	st.push(23)
+	st.pop() // returns 23.
 }
